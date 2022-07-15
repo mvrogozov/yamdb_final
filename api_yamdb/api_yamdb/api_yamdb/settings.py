2b2c0a82ff9a44029f848c7f0113b1f8b0f6bfc2
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-07-15 20:56:31 +0300

    workflow

--- api_yamdb/api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/api_yamdb/settings.py
@@ -58,10 +58,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('POSTGRES_USER'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': os.getenv('DB_PORT')
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
